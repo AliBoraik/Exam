0fd221af0945e79ce35c3e9f3d936663f3963f7d
Salavat <72506866+SalavatKhR@users.noreply.github.com>
2023-01-13 19:50:10 +0300

    layout done

--- Tic-tac-toe/TicTacToe.Client/src/App.js
+++ Tic-tac-toe/TicTacToe.Client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import useToken from './components/Login/useToken';
 import TicTacToe from "./components/TicTacToe";
@@ -8,15 +9,19 @@ import ListGame from "./components/ListGames"
 
 function App() {
     const { token, setToken } = useToken();
-    
-    if(!token) {
-        return <Login setToken={setToken} />
-    }
+
+   /* if(!token) {
+        navigate("/login")
+    }*/
   return (
-    <div style={{ margin: '4% 30%' }}>
-        <TicTacToe token={token}/>
-        {/*<ListGame token={token}/>*/}
-    </div>
+      <Switch>
+          <div style={{ margin: '4% 30%' }}>
+              <Route path="/" component={ListGame}/>
+              <Route path="/game" component={TicTacToe}/>
+              <Route path="/login" component={Login}/>
+          </div>
+      </Switch>
+
   );
 }
 
